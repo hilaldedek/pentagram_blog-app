fbc565d23b679a828b3cad18e3b2b65d35aaa7f6
hilal dedek <hdedek03@gmail.com>
2023-12-12 23:01:32 +0300

    added login view

--- app.py
+++ app.py
@@ -9,9 +9,14 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 
 # DATABASE
-connect("deneme", host="mongodb://localhost:27017/?directConnection=true")
+client = MongoClient("mongodb://localhost:27017/?directConnection=true")
+database = client["pentagram_db"]
+collection = database["user"]
+
+connect("pentagram_db", host="mongodb://localhost:27017/?directConnection=true")
+
 try:
-    connect("deneme", host="mongodb://localhost:27017/?directConnection=true")
+    connect("pentagram_db", host="mongodb://localhost:27017/?directConnection=true")
     print("succesfully connected!")
 except Exception as error:
     print("unseccesfully process:", error)
@@ -25,8 +30,14 @@ class User(UserMixin, Document):
     username = StringField(required=True, max_length=64, unique=True)
     email = EmailField(required=True, unique=True)
     password = StringField(required=True)
+
     # unique_emails = set()
     # Password Hashing
+    def get_user_with_username(self, username):
+        if self.username == username:
+            return self
+        else:
+            return None
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -41,8 +52,19 @@ def home():
     return "Home Page"
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        data = request.get_json()
+        username = data.get("username")
+        results = collection.find_one({"username": f"{username}"})
+        if results is not None:
+            if check_password_hash(results.get("password"), data.get("password")):
+                print("USER LOGIN")
+            else:
+                print("PASSWORD WRONG")
+        else:
+            print("USER NOT FOUND")
     return "Login Page"
 
 
@@ -57,6 +79,7 @@ def register():
         )
         new_user.set_password(data.get("password"))
         new_user.save()
+        print("USER REGISTER")
+        login()
         return jsonify({"message": "Data saved successfully"})
-
     return "Register Page"
