bab5dd2ce9fc8052c187b0cec8e22e690c6ea7aa
hilal dedek <hdedek03@gmail.com>
2024-01-01 20:24:50 +0300

    feat: add post_list_view and update models

--- api/models/comment_vote.py
+++ api/models/comment_vote.py
@@ -4,7 +4,9 @@ from models.post import Post
 
 
 class Comment_vote(Document):
+    _id = IntField()
     person = ReferenceField(User)
     postID = ReferenceField(Post)
-    vote = IntField()
+    vote = BooleanField()
     comment = StringField()
+    updated=BooleanField()
