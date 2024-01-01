8d702f768e1e8abbd3d500836e25a7cdcc39198d
Hilal Dedek <95539281+hilaldedek@users.noreply.github.com>
2024-01-01 15:08:08 +0300

    Comment (#4)
    * feat: added post view
    
    * feat:_id rearranged
    
    * feat: added comment-vote and post-detail endpoints

--- models/post.py
+++ models/post.py
@@ -1,9 +1,11 @@
 from mongoengine import *
 from models.user import User
 
+
 class Post(Document):
     author = ReferenceField(User)
     title = StringField(required=True)
     content = StringField(required=True)
-    image = ImageField()
+    image = ImageField(required=False)
     dateTime = DateTimeField()
+    _id = IntField()
