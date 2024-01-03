b4c610ea4cd70cf35533892569de43515e1f19e9
Hilal Dedek <95539281+hilaldedek@users.noreply.github.com>
2024-01-03 11:22:18 +0300

    Remove (#6)
    * feat: added comment-vote and post-detail endpoints
    
    * feat:client folder added, files moved to api folder
    
    * feat: merge dev branch

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import os
 import secrets
 from flask import Flask, jsonify, request
@@ -18,14 +18,31 @@ from flask_jwt_extended import (
 )
 import requests
 from config import config
+import redis
 
+ACCESS_EXPIRES = timedelta(hours=1)
 
 app = Flask(__name__)
 
+jwt_redis_blocklist = redis.StrictRedis(
+    host="localhost", port=6379, db=0, decode_responses=True
+)
 # Setup the Flask-JWT-Extended extension
 app.config.from_object(config)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = ACCESS_EXPIRES
+app.config["JWT_BLACKLIST_ENABLED"] = True
+app.config["JWT_BLACKLIST_TOKEN_CHECKS"] = ["access"]
 jwt = JWTManager(app)
 token_blacklist = set()
+
+
+@jwt.token_in_blocklist_loader
+def check_if_token_is_revoked(jwt_header, jwt_payload: dict):
+    jti = jwt_payload["jti"]
+    token_in_redis = jwt_redis_blocklist.get(jti)
+    return token_in_redis is not None
+
+
 
 # DATABASE
 client = MongoClient("mongodb://localhost:27017/?directConnection=true")
@@ -33,7 +50,6 @@ database = client["pentagram_db"]
 collectionUser = database["user"]
 collectionPost = database["post"]
 collectionComment_vote = database["comment_vote"]
-
 
 connect("pentagram_db", host="mongodb://localhost:27017/?directConnection=true")
 
@@ -87,13 +103,6 @@ def login():
         return jsonify({"message": "Invalid username"}), 401
 
 
-@app.route("/protected", methods=["GET"])
-@jwt_required()
-def protected():
-    current_user = get_jwt_identity()
-    return jsonify(logged_in_as=current_user), 200
-
-
 # Register Page
 @app.route("/register", methods=["POST"])
 def register():
@@ -120,15 +129,13 @@ def register():
 @app.route("/logout", methods=["POST"])
 @jwt_required()
 def logout():
-    jti = get_jwt()["jti"]
-    token_blacklist.add(
-        jti
-    )  # Logout of the user who wants to log out by adding them to the blacklist
-    return jsonify({"message": "Successfully logged out"}), 200
+    jti = get_jwt_identity()
+    jwt_redis_blocklist.set(jti, "true", ex=app.config["JWT_ACCESS_TOKEN_EXPIRES"])
+    return jsonify(message="cikis  basarili"), 200
 
 
 # Post Creation
-@app.route("/post", methods=["POST", "GET"])
+@app.route("/post", methods=["POST"])
 @jwt_required()
 def post():
     data = request.get_json()
@@ -146,9 +153,6 @@ def post():
     return jsonify({"message": "Post created successfully"})
 
 
-# FARKLI KİŞİ POST ATARKEN ERROR
-
-
 @app.route("/post-detail/<int:post_id>", methods=["DELETE", "PATCH", "GET"])
 @jwt_required()
 def detail_post(post_id):
@@ -156,43 +160,81 @@ def detail_post(post_id):
     results = collectionPost.find(
         {"author": f"{current_user_id}", "_id": post_id}
     )  # The value from the url was searched in the database
+    print("RESULTS:", results)
     results_list = list(results)
+    print("RESULT LIST: ", results_list)
     if request.method == "DELETE":
         if results_list.__len__() != 0:
             collectionPost.delete_one(
                 {"_id": post_id}
             )  # deleting the data whose id is given
+            collectionComment_vote.delete_many(
+                {"postID": post_id}
+            )  # If the post is deleted, the comments made on the post will be deleted.
             return jsonify({"message": "Post deleted successfully"})
         else:
             return jsonify({"message": "Post is not found"})
     elif request.method == "GET":
         json_data = jsonify(results_list)
+
+
         if json_data is not None:
             return json_data
         else:
             return jsonify({"message": "Post is not found"})
 
     elif request.method == "PATCH":
-        data = requests.get_json()
-        print(data)
+        data = request.get_json()
+        print("DATA: ", data)
+        # ÇALIŞMIYOR
+        for key, value in data.items():
+            if key != "_id":
+                results[key] = value
+                collectionPost.update_one({"_id": post_id}, {"$set": results})
 
 
 @app.route("/comment-vote/<int:post_id>", methods=["POST"])
 @jwt_required()
 def comment_vote(post_id):
-    data = request.get_json()
-    current_user_id = get_jwt_identity()  # current user
-    result = collectionPost.find_one({"_id": post_id})
-    if result is not None:
-        new_comment_vote = Comment_vote(
-            person=current_user_id,
-            postID=post_id,
-            comment=data.get("comment"),
-            vote=data.get("vote"),
-        )
-        print("POSTID: ",request.args.get("post_id"))
-        meta = {"collection": "comment_vote"}  # Collection name to save the user to
-        new_comment_vote.save()
-        return jsonify({"message": "Comment Vote saved successfully"})
+    # Saving comments and votes on the post
+    if request.method == "POST":
+        data = request.get_json()
+        current_user_id = get_jwt_identity()  # current user
+        result = collectionPost.find_one({"_id": post_id})
+        if result is not None:
+            new_comment_vote = Comment_vote(
+                person=current_user_id,
+                postID=post_id,
+                comment=data.get("comment"),
+                vote=data.get("vote"),
+            )
+            print("POSTID: ", request.args.get("post_id"))
+            meta = {"collection": "comment_vote"}  # Collection name to save the user to
+            new_comment_vote.save()
+            return jsonify({"message": "Comment Vote saved successfully"})
+        else:
+            return jsonify({"message": "Post is not found"})
+
+
+@app.route("/comment-vote/list", methods=["GET"])
+def comment_vote_list_view():
+    # All users can list comments and votes
+    results = collectionComment_vote.find({}, projection={"_id": 0})
+    results_list = list(results)
+    json_data = results_list
+    print(json_data)
+    if json_data is not None:
+        return jsonify(json_data)
     else:
         return jsonify({"message": "Post is not found"})
+
+
+@app.route("/post/list", methods=["GET"])
+def post_list_view():
+    # All users can list all comments and votes
+    results = list(collectionPost.find({}))
+    print("RESULTS: ", results)
+    json_data = jsonify(results)
+    print(json_data)
+    if json_data is not None:
+        return json_data
