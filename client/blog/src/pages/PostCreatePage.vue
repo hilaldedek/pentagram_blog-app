7013955f5b47f22b048b4fe842605dcc252ff291
hilal dedek <hdedek03@gmail.com>
2024-02-17 13:35:04 +0300

    feat:The test of the login section was written

--- client/blog/src/pages/PostCreatePage.vue
+++ client/blog/src/pages/PostCreatePage.vue
@@ -55,7 +55,7 @@ export default {
           headers: {
             "Content-Type": "application/json",
             Authorization: `Bearer ${token}`,
-            "Access-Control-Allow-Origin": "http:/localhost:8080",
+            "Access-Control-Allow-Origin": "http:/localhost:80",
           },
           body: JSON.stringify({
             title: this.title,
